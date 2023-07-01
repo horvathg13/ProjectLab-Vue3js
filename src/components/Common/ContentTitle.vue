c4bf37daa2a00b4e26c6e43dd75708ab6402dc7b
Gabor_Horvath <horvathgabor338@gmail.com>
2023-07-01 16:48:42 +0200

    Shakeing evelope developed.

--- src/components/Common/ContentTitle.vue
+++ src/components/Common/ContentTitle.vue
@@ -15,13 +15,49 @@ export default{
 </script>
 
 <template>
-    <div class="content-title" v-if="h1Title !== 'Users'">
-        <h1>{{h1Title}}</h1>
-    </div>
-    <div class="content-title users" v-if="h1Title == 'Users'">
-        <h1>{{h1Title}}</h1>
+    <div class="content-title-container">
+        <div class="content-title" v-if="h1Title !== 'Users'">
+            <h1>{{h1Title}}</h1>
+        </div>
+        <div class="content-title users" v-if="h1Title == 'Users'">
+            <h1>{{h1Title}}</h1>
+        </div>
+        <div class="message-container">
+            <div class="ui large red left pointing label"><i class="envelope icon"></i>1</div>
+        </div>
     </div>
 </template>
 
-<style>
+<style scoped>
+    .ui.label{
+        width: max-content;
+        z-index: 99;
+        cursor: pointer;
+        top:30px;
+        
+        
+    }
+    .message-container{
+        left:60%;
+    }
+    .content-title-container{
+        width: 100%;
+        z-index: 999;
+        
+        display: flex;
+    }
+
+    .message-container:hover {
+        animation: shake 0.4s;
+        animation-iteration-count: 1;
+    }
+
+@keyframes shake {
+    0% { transform: translate(1px, 1px) rotate(0deg); }
+    20% { transform: translate(-3px, 0px) rotate(1deg); }
+    30% { transform: translate(3px, 2px) rotate(0deg); }
+    60% { transform: translate(-2px, 1px) rotate(0deg); }
+    70% { transform: translate(1px, 1px) rotate(-1deg); }
+    100% { transform: translate(1px, -2px) rotate(-1deg); }
+}
 </style>

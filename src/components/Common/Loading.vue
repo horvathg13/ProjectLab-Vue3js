a7dc8b1928cba2c5c21f8325c1af54b35a2ecc52
Gabor_Horvath <horvathgabor338@gmail.com>
2023-08-19 17:39:57 +0200

    Loader common component developed.

---
+++ src/components/Common/Loading.vue
@@ -0,0 +1,38 @@
+<script>
+export default{
+    props:{
+        
+    },
+    watch:{
+        
+    },
+    data(){
+
+    },
+    methods:{
+
+    },
+}
+
+</script>
+
+<template>
+    <div class="ui segment" >
+        <div class="ui active dimmer">
+            <div class="ui small text loader">Loading</div>
+        </div>
+        <p></p>
+    </div>
+</template>
+
+<style scoped>
+    .ui.segment{
+        position: absolute;
+        width:100%;
+        height: 100% !important;
+        
+    }
+    .ui.dimmer{
+        background-color: rgba(0, 0, 0, 0.75);
+    }
+</style>

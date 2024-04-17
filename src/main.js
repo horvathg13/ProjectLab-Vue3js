c26bc128b2be56d5bb8886ed70d720ab6c3120c3
Gabor_Horvath <horvathgabor338@gmail.com>
2024-04-17 12:31:19 +0200

    main.js modified

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ import {store} from './VuexStore'
 import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css';
 import VueClickAway from "vue3-click-away";
+import ServiceClient from "@/ServiceClient";
 
-
+function dataPreLoader(){
+    return ServiceClient.getUserRoles().then(()=>app.use(router).mount('#app'))
+}
 const app = createApp(App)
-app.use(router).use(store).use(VueClickAway).component('VueDatePicker', VueDatePicker);
+app.use(store)
+    .use(dataPreLoader)
+    .use(VueClickAway)
+    .component('VueDatePicker', VueDatePicker);
 
-
-app.mount('#app')
